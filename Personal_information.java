46898fc400cdb7ea8007dd50bf2225df1ebc95dd
sahel-chowdhury <mdsahelchowdhuryabir@gmail.com>
2025-09-26 23:11:18 +0600

    Add files via upload

---
+++ Personal_information.java
@@ -0,0 +1,19 @@
+public class Personal_information {
+    public static void main(String[] args) {   
+        String name = "Md.Sahel Chowdhury";   
+        String id = "E251015";   
+        String age = "21";   
+        String mail = "mdsahelchowdhuryabir@gmail.com";   
+        String phone = "01321875202";   
+        String address = "Raozan,Chittagong";   
+ 
+        System.out.println("Name: " + name);   
+        System.out.println("ID: " + id);   
+        System.out.println("Age: " + age);   
+        System.out.println("Mail: " + mail);   
+        System.out.println("Phone: " + phone);   
+        System.out.println("Address: " + address);  
+    }
+    
+}
+    
