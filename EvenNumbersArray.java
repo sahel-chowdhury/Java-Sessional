e69420aeb43f7c9df3f3e73a8e0836e265729f76
sahel-chowdhury <mdsahelchowdhuryabir@gmail.com>
2025-10-03 23:29:32 +0600

    Add files via upload

---
+++ EvenNumbersArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class EvenNumbersArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        int count = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                count++;
+            }
+        }
+
+        int[] evens = new int[count];
+        int index = 0;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                evens[index] = i;
+                index++;
+            }
+        }
+
+      System.out.println("Even numbers in the range:");
+        for (int i = 0; i < evens.length; i++) {
+            System.out.print(evens[i] + " ");
+        }
+        sc.close();
+    }
+}
---
+++ PrimeNumbersArray.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class PrimeNumbersArray {
+    
+    // Method to check prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        int count = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                count++;
+            }
+        }
+
+        int[] primes = new int[count];
+        int index = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                primes[index] = i;
+                index++;
+            }
+        }
+
+
+        System.out.println("Prime numbers in the range:");
+        for (int i = 0; i < primes.length; i++) {
+            System.out.print(primes[i] + " ");
+        }
+
+        sc.close();
+    }
+}
